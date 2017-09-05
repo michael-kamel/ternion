5c9cd18379c046b698a5c99737126a62a255e796
Michael Kamel <michael.kamel.2020@gmail.com>
2017-09-05 15:21:19 +0200

    tests batch #7

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -27,4 +27,149 @@ describe('Manager tests', () =>
             expect(() => new Manager('test', new EventEmitter(), 'test')).not.toThrow()
         })
     })
+    describe('Method Tests', () =>
+    {
+        test('Should return on non existing message', () =>
+        {
+            const mocke = jest.fn()
+            const mockb = jest.fn()
+            const eventSource = {
+                sockets:{
+                    connected:{
+                        'test': {
+                            broadcast:{emit:mockb},
+                            emit:mocke
+                        }
+                    }
+                }
+            }
+            const emitter = new EventEmitter
+            const identifier = 'test'
+            const manager = new Manager(eventSource, emitter, identifier)
+            const data = undefined
+            emitter.on = (dat, func) => func(data)
+            manager._listen()
+            expect(mocke).not.toHaveBeenCalled()
+            expect(mockb).not.toHaveBeenCalled()
+        })
+        test('Should return on no msgtype', () =>
+        {
+            const mocke = jest.fn()
+            const mockb = jest.fn()
+            const eventSource = {
+                sockets:{
+                    connected:{
+                        'test': {
+                            broadcast:{emit:mockb},
+                            emit:mocke
+                        }
+                    }
+                }
+            }
+            const emitter = new EventEmitter
+            const identifier = 'test'
+            const manager = new Manager(eventSource, emitter, identifier)
+            const data = {}
+            emitter.on = (dat, func) => func(data)
+            manager._listen()
+            expect(mocke).not.toHaveBeenCalled()
+            expect(mockb).not.toHaveBeenCalled()
+        })
+        test('Should return on no msgdata', () =>
+        {
+            const mocke = jest.fn()
+            const mockb = jest.fn()
+            const eventSource = {
+                sockets:{
+                    connected:{
+                        'test': {
+                            broadcast:{emit:mockb},
+                            emit:mocke
+                        }
+                    }
+                }
+            }
+            const emitter = new EventEmitter
+            const identifier = 'test'
+            const manager = new Manager(eventSource, emitter, identifier)
+            const data = {msgType:'t'}
+            emitter.on = (dat, func) => func(data)
+            manager._listen()
+            expect(mocke).not.toHaveBeenCalled()
+            expect(mockb).not.toHaveBeenCalled()
+        })
+        test('Should return on no msgdata', () =>
+        {
+            const mocke = jest.fn()
+            const mockb = jest.fn()
+            const eventSource = {
+                sockets:{
+                    connected:{
+                        'test': {
+                            broadcast:{emit:mockb},
+                            emit:mocke
+                        }
+                    }
+                }
+            }
+            const emitter = new EventEmitter
+            const identifier = 'test'
+            const manager = new Manager(eventSource, emitter, identifier)
+            const data = {msgType:'t', msgData:'d'}
+            emitter.on = (dat, func) => func(data)
+            manager._listen()
+            expect(mocke).not.toHaveBeenCalled()
+            expect(mockb).not.toHaveBeenCalled()
+        })
+        test('Should broadcast', () =>
+        {
+            const mock = jest.fn()
+            const eventSource = {
+                sockets:{
+                    connected:{
+                        'test': {
+                            broadcast:{emit:mock},
+                        }
+                    }
+                }
+            }
+            const emitter = new EventEmitter
+            const identifier = 'test'
+            const manager = new Manager(eventSource, emitter, identifier)
+            const data = {msgType:'t', msgData:'d', broadcast:true, ids:['test']}
+            emitter.on = (dat, func) => func(data)
+            manager._listen()
+            expect(mock).toHaveBeenCalledTimes(1)
+        })
+        test('Should respond to some', () =>
+        {
+            const mock1 = jest.fn()
+            const mock2 = jest.fn()
+            const mock3 = jest.fn()
+            const eventSource = {
+                sockets:{
+                    connected:{
+                        'test1': {
+                            emit:mock1,
+                        },
+                        'test2': {
+                            emit:mock2,
+                        },
+                        'test3': {
+                            emit:mock3,
+                        }
+                    }
+                }
+            }
+            const emitter = new EventEmitter
+            const identifier = 'test'
+            const manager = new Manager(eventSource, emitter, identifier)
+            const data = {msgType:'t', msgData:'d', ids:['test1', 'test2']}
+            emitter.on = (dat, func) => func(data)
+            manager._listen()
+            expect(mock1).toHaveBeenCalledTimes(1)
+            expect(mock2).toHaveBeenCalledTimes(1)
+            expect(mock3).not.toHaveBeenCalled()
+        })
+    })
 })
