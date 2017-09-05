9510ad1fe31a014f349b35ad2178fd4050b34082
Michael Sherif <michael.kamel.2020@gmail.com>
2017-09-05 06:23:30 +0200

    tests batch #5

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     verbose: true,
     collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.js', '!src/factories/**/*']
+    collectCoverageFrom: ['src/**/*.js', '!src/factories/**/*', '!src/default.js']
 }
